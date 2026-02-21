c241f9f419da9ec3a19e70905d59dbacfbe7b943
karmette <yaya.ny@proton.me>
2026-02-21 14:34:00 -0600

    wip weapon switch

--- src/ui/weapon_slot.gd
+++ src/ui/weapon_slot.gd
@@ -1,18 +1,23 @@
 extends Control
 
 @export var selected: bool = false
+@export var weapon_texture: Texture2D
+
 @onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var sprite: TextureRect = $Slot/Weapon
+
 var temp_selected: bool = false
 signal selected_changed
 
 func _ready() -> void:
 	selected_changed.connect(select)
+	sprite.texture = weapon_texture
 
 func select():
 	if selected:
-		anim.play_backwards("select")
-	else:
 		anim.play("select")
+	else:
+		anim.play_backwards("select")
 
 
 func _process(_delta):
--- src/ui/weapon_switcher.gd
+++ src/ui/weapon_switcher.gd
@@ -1,10 +1,41 @@
 extends Control
 
 var slot: PackedScene = preload("uid://hpt5itklu07h")
+var fork_texture: Texture2D = preload("uid://dgop0rkdkfmt6")
+var knife_texture: Texture2D = preload("uid://cev5gora74jq3")
+var cheese_texture: Texture2D = preload("uid://c7alhccw35b46")
 
-enum Weapon{FORK, KNIFE, BLUE_CHEESE}
+@onready var cd: Timer = $SwitchCooldown
+var can_switch: bool = true
+
+
+enum Weapon{KNIFE, FORK, BLUE_CHEESE}
 
 var current_weapon: Weapon = Weapon.KNIFE
+var weapon_slots: Dictionary[Weapon, Control] = {}
+
+func _ready() -> void:
+	cd.timeout.connect(_on_cd_timeout)
+
+	# knife
+	weapon_slots[Weapon.KNIFE] = slot.instantiate()
+	weapon_slots[Weapon.KNIFE].weapon_texture = knife_texture
+
+	# fork
+	weapon_slots[Weapon.FORK] = slot.instantiate()
+	weapon_slots[Weapon.FORK].weapon_texture = fork_texture
+
+	# cheese
+	weapon_slots[Weapon.BLUE_CHEESE] = slot.instantiate()
+	weapon_slots[Weapon.BLUE_CHEESE].weapon_texture = cheese_texture
+	var offset: int = 100
+	for i in weapon_slots:
+		var weapon: Control = weapon_slots.get(i)
+		add_child(weapon)
+		weapon.position = Vector2(100+offset, 900+(offset/10.0))
+		offset += 100
+		weapon.scale *= 2
+
 
 func add_weapon(weapon_name: String):
 	if not Constants.WEAPONS.has(weapon_name):
@@ -16,12 +47,22 @@ func add_weapon(weapon_name: String):
 	slot_node.set_anchors_and_offsets_preset(Control.PRESET_BOTTOM_LEFT)
 
 func switch_weapon(weapon: Weapon):
-	pass
-
+	for i in weapon_slots:
+		if i != weapon:
+			weapon_slots.get(i).selected = false
+		else:
+			weapon_slots.get(i).selected = true
+	cd.start()
+	can_switch = false
+		
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("select_1"): # TODO: add unlocked weapons code later
-		switch_weapon(Weapon.KNIFE)
-	elif event.is_action_pressed("select_2"):
-		switch_weapon(Weapon.FORK) 
-	elif event.is_action_pressed("select_3"):
-		switch_weapon(Weapon.BLUE_CHEESE) 
+	if can_switch:
+		if event.is_action_pressed("select_1"): # TODO: add unlocked weapons code later
+			switch_weapon(Weapon.KNIFE)
+		elif event.is_action_pressed("select_2"):
+			switch_weapon(Weapon.FORK) 
+		elif event.is_action_pressed("select_3"):
+			switch_weapon(Weapon.BLUE_CHEESE) 
+
+func _on_cd_timeout() -> void:
+	can_switch = true
