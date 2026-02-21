64b4011206f8dfc7181c791acc6dfd7a0e273a81
karmette <yaya.ny@proton.me>
2026-02-21 12:05:50 -0600

    collision updates and wave spawning

--- src/autoloads/constants.gd
+++ src/autoloads/constants.gd
@@ -3,3 +3,12 @@ extends Node
 const DIALOG = {
 	"cutscene_one":"res://assets/json/dialogue/cheese.json"
 }
+
+const LEVELS: Dictionary[String, PackedScene] = {
+	"sewers": preload("uid://c86shht007ngr"),
+}
+
+const ENEMIES_EASY: Dictionary[String, PackedScene] = {
+	"small_rat":preload("uid://dqfe3qn5ji0i5"),
+	#"zombie_rat":""
+}
